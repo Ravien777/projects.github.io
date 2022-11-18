9247c6c6d1287ed26f08f72ed0b8e13aa83ce29a
Ravien777 <r.sewpal@unasat.sr>
2022-11-18 09:12:35 -0300

    Add nav active change on scroll

--- assets/js/minibar-virtual.js
+++ assets/js/minibar-virtual.js
@@ -249,3 +249,29 @@ $(document).ready(function () {
   };
   counterInit();
 });
+
+let section = document.querySelectorAll(".scroll-effect");
+let lists = document.querySelectorAll(".menu-item");
+function activeLink(li) {
+  lists.forEach((item) => item.classList.remove("active"));
+  li.classList.add("active");
+}
+lists.forEach((item) =>
+  item.addEventListener("click", function () {
+    activeLink(this);
+  })
+);
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY + 300;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      const target = document.querySelector(`[href='#${id}']`).parentElement;
+      activeLink(target);
+    }
+  });
+};
